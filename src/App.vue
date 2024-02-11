7c99aa10701c463fcd0ceefd1c6e8315aaf6cebc
RDPA-GitHub <ronald.micros@gmail.com>
2024-02-11 18:18:59 -0300

    Creacion de Proyecto todo list con Vue, Fecha: 11/02/2024

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todo msg="Todo List"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      datos: null
+    }
+  },
   components: {
-    HelloWorld
+    Todo
+  },
+  methods:{
+    async peticion (){
+
+      try {
+        let Servidor = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(JSON.stringify(Servidor.data, undefined, 3));
+        this.datos = Servidor;
+      } catch (error) {
+        console.log(error.message)
+      }
+
+    }, 
+    getUsers() {
+      axios({
+        url: 'https://jsonplaceholder.typicode.com/users',
+        method: 'GET',
+      }).then((Respuesta) => console.log(Respuesta.data))
+        .catch((error) => console.log(error.message));
+    }
+  },
+  mounted(){
+    this.getUsers();
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ color: rgb(56, 56, 56);
+ text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center mt-4">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+ body{
+  background-color: orange;
+ }
+</style>
