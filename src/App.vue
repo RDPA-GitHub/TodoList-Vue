16e5edd86da17700f656e370aee2b60a7f4a2396
RDPA-GitHub <ronald.micros@gmail.com>
2024-02-12 02:19:01 -0300

    Se instalo JSON-sever para manipulacion del Listado nuevo con otra peticion de Axios, Fecha: 12/02/2024

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="d-flex justify-content-center">
       <Todo msg="Listado" />
       <div class="d-flex align-items-center mt-2">
-        <button class="ms-3" :class="['btn', datos.length > 0 ? 'btn-secondary disabled' : 'btn-success', 'fw-bold']"
+        <button title="Mostrando Usuarios Estáticos" class="ms-3" :class="['btn', datos.length > 0 ? 'btn-secondary' : 'btn-success', 'fw-bold']"
         @click="Actualizacion()">
         ⚡
       </button>
@@ -13,14 +13,14 @@
     <div class="container">
       <div class="row mt-3  ">
         <div class="col-md-12 table-responsive">
-          <table class="table table-dark table-striped">
+          <table v-if="datos.length > 0" class="table table-dark table-striped">
             <thead class="table-active">
               <tr class="text-center">
 
                 <th>ID</th>
                 <th>Nombre</th>
                 <th>Alias </th>
-                <th>Acciones</th>
+
               </tr>
             </thead>
             <tbody>
@@ -29,6 +29,33 @@
                 <td>{{ value.id }}</td>
                 <td>{{ value.name }}</td>
                 <td>{{ value.username }}</td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="row mt-3  ">
+        <div class="col-md-12 table-responsive">
+          <table class="table table-dark table-striped">
+            <thead class="table-active">
+              <tr class="text-center">
+
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="text-center" v-for="(value, index) in usuarios" :key="index">
+
+                <td>{{ value.id }}</td>
+                <td>{{ value.title }}</td>
+                <td :class="[value.active ? 'text-success fw-bold' : 'text-danger fw-bold']">
+                  {{ value.active ? 'Activo' : 'Inactivo'}}
+                </td>
 
                 <td>
                   <button class="btn btn-outline-warning me-1 fw-bold">
@@ -44,6 +71,7 @@
           </table>
         </div>
       </div>
+
     </div>
 
 
@@ -76,18 +104,26 @@ export default {
     },
     getUsers() {
       axios({
-        url: 'https://jsonplaceholder.typicode.com/users',
+        url: 'http://localhost:3000/posts',
         method: 'GET'
-      }).then((Respuesta) => console.log(Respuesta.data))
+      }).then((Respuesta) => {
+        this.usuarios = Respuesta.data;
+        console.log(Respuesta.data);
+      })
         .catch((error) => console.log(error.message));
     },
     Actualizacion() {
-      this.peticion();
+      if(this.datos.length > 0){
+        this.datos = [];
+      }else{
+        this.peticion();
+      }
     }
   },
   data() {
     return {
-      datos: []
+      datos: [],
+      usuarios: []
     }
   },
   mounted() {
