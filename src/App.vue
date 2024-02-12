08cc9613ba64cd97c44fe38fd13c9d74ae7af710
RDPA-GitHub <ronald.micros@gmail.com>
2024-02-12 01:31:07 -0300

    Creacion de Listado con petición por axios a JsonPlaceholder, Fecha: 12/02/2024

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,54 @@
 <template>
   <div id="app">
-    <Todo msg="Todo List"/>
+    <div class="d-flex justify-content-center">
+      <Todo msg="Listado" />
+      <div class="d-flex align-items-center mt-2">
+        <button class="ms-3" :class="['btn', datos.length > 0 ? 'btn-secondary disabled' : 'btn-success', 'fw-bold']"
+        @click="Actualizacion()">
+        ⚡
+      </button>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div class="row mt-3  ">
+        <div class="col-md-12 table-responsive">
+          <table class="table table-dark table-striped">
+            <thead class="table-active">
+              <tr class="text-center">
+
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Alias </th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="text-center" v-for="(value, index) in datos" :key="index">
+
+                <td>{{ value.id }}</td>
+                <td>{{ value.name }}</td>
+                <td>{{ value.username }}</td>
+
+                <td>
+                  <button class="btn btn-outline-warning me-1 fw-bold">
+                    Edit
+                  </button>
+                  <button class="btn btn-outline-danger ms-1 fw-bold">
+                    Delete
+                  </button>
+                </td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+
+
+
   </div>
 </template>
 
@@ -10,35 +58,39 @@ import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      datos: null
-    }
-  },
   components: {
-    Todo
+    Todo,
   },
-  methods:{
-    async peticion (){
+  methods: {
+    async peticion() {
 
       try {
         let Servidor = await axios.get('https://jsonplaceholder.typicode.com/users');
-        console.log(JSON.stringify(Servidor.data, undefined, 3));
-        this.datos = Servidor;
+
+        this.datos = Servidor.data;
+        console.log(Servidor.data);
       } catch (error) {
         console.log(error.message)
       }
 
-    }, 
+    },
     getUsers() {
       axios({
         url: 'https://jsonplaceholder.typicode.com/users',
-        method: 'GET',
+        method: 'GET'
       }).then((Respuesta) => console.log(Respuesta.data))
         .catch((error) => console.log(error.message));
+    },
+    Actualizacion() {
+      this.peticion();
     }
   },
-  mounted(){
+  data() {
+    return {
+      datos: []
+    }
+  },
+  mounted() {
     this.getUsers();
   }
 }
@@ -46,7 +98,7 @@ export default {
 
 <style>
 #app {
- color: rgb(56, 56, 56);
- text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  color: rgb(56, 56, 56);
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './style/Style.css';
 
+
 Vue.config.productionTip = false
 
 new Vue({
